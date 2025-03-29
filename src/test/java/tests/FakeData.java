62b9baa5b916486dc85d502fec68b5c055bd5a98
LizaRoma-lab <Elizaveta.Romanova.m@yandex.ru>
2025-03-29 21:56:32 +0300

    added faker

---
+++ src/test/java/tests/FakeData.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class FakeData {
+    Faker faker = new Faker(new Locale("en-GB"));
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            gender = faker.demographic().sex(),
+            userNumber = faker.numerify("##########"),
+            yearOfBirth = String.valueOf(faker.number().numberBetween(1900, 2020)),
+            monthOfBirth = faker.options().option("January", "August", "May", "February", "March"),
+            dayOfBirth = String.format("%02d", faker.number().numberBetween(1, 29)),
+            subject = faker.options().option("Computer Science", "Chemistry", "Maths", "English", "Biology"),
+            hobbies = faker.options().option("Sports", "Reading", "Music"),
+            picture = "cat.jfif",
+            streetAddress = faker.address().streetAddress(),
+            state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan"),
+            city = findCityByState(state);
+
+    public String findCityByState(String state) {
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> "";
+        };
+    }
+}
---
+++ src/test/java/tests/RegistrationWithFakerTests.java
@@ -0,0 +1,85 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+public class RegistrationWithFakerTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    FakeData fakeData = new FakeData();
+
+    String firstName = fakeData.firstName,
+            lastName = fakeData.lastName,
+            userEmail = fakeData.userEmail,
+            gender = fakeData.gender,
+            userNumber = fakeData.userNumber,
+            dayOfBirth = fakeData.dayOfBirth,
+            monthOfBirth = fakeData.monthOfBirth,
+            yearOfBirth = fakeData.yearOfBirth,
+            subject = fakeData.subject,
+            hobbies = fakeData.hobbies,
+            picture = fakeData.picture,
+            streetAddress = fakeData.streetAddress,
+            state = fakeData.state,
+            city = fakeData.city;
+
+    @Test
+    void successfulRegistrationTest() {
+
+        registrationPage.openPage()
+                .removeBanners()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(userEmail)
+                .setGender(gender)
+                .setUserNumber(userNumber)
+                .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
+                .setSubjects(subject)
+                .setHobbies(hobbies)
+                .setPicture(picture)
+                .setAddress(streetAddress)
+                .setState(state)
+                .setCity(city);
+
+        registrationPage.checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", userEmail)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", userNumber)
+                .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobbies)
+                .checkResult("Picture", picture)
+                .checkResult("Address", streetAddress)
+                .checkResult("State and City", state + " " + city);
+    }
+
+    @Test
+    void successfulMinRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setGender(gender)
+                .setUserNumber(userNumber)
+                .setSubmit();
+
+        registrationPage.checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", userNumber)
+                .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth);
+    }
+
+    @Test
+    void unsuccessfulRegistrationWithoutGenderTest() {
+
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserNumber(userNumber)
+                .setSubmit();
+
+        registrationPage.visibleFirstName();
+    }
+
+}
